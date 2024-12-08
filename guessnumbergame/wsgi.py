fdb1983b07b8f3dfd8e3222ac2665e913aaefe1e
Yevhenii <146078178+EvheniiOrza@users.noreply.github.com>
2024-12-08 16:43:31 +0200

    main

--- guessnumbergame/wsgi.py
+++ guessnumbergame/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
+import gunicorn
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guessnumbergame.settings')
